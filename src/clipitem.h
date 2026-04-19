5dc34ab047d76405ec8216efbd0b142cb0a1c84b
Eliene-byte <elienedantas123456@gmail.com>
2026-04-19 11:09:08 -0300

    fix: adiciona declaracao paint() no ClipItem

--- src/clipitem.h
+++ src/clipitem.h
@@ -1,14 +1,18 @@
 ﻿#pragma once
 #include <QGraphicsRectItem>
-#include <QBrush>
 #include <QObject>
+
+class QPainter;
+class QStyleOptionGraphicsItem;
 
 class ClipItem : public QObject, public QGraphicsRectItem
 {
     Q_OBJECT
 public:
     ClipItem(qreal x, qreal y, qreal w, qreal h, int pixelsPerSecond);
-    
+    void setFilePath(const QString &p){ m_filePath = p; }
+    QString filePath() const { return m_filePath; }
+
 signals:
     void requestSnap(ClipItem* clip);
 
@@ -16,9 +20,11 @@ protected:
     void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
 
 private:
     bool m_dragging;
     QPointF m_dragStartPos;
     int m_pixelsPerSecond;
+    QString m_filePath;
 };
