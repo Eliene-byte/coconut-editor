78729f82ca5c977335feed97edf6650e90a0c509
Eliene-byte <elienedantas123456@gmail.com>
2026-04-19 11:09:23 -0300

    fix: headers para MSVC

--- src/timelinewidget.h
+++ src/timelinewidget.h
@@ -1,7 +1,9 @@
 ﻿#pragma once
 #include <QGraphicsView>
-#include <QGraphicsScene>
 
+class QGraphicsScene;
+class QGraphicsLineItem;
+class QTimer;
 class ClipItem;
 
 class TimelineWidget : public QGraphicsView
@@ -10,15 +12,25 @@ class TimelineWidget : public QGraphicsView
 public:
     explicit TimelineWidget(QWidget *parent = nullptr);
     void addClip(qreal startSeconds, qreal durationSeconds, int track);
+    void addClipWithFile(qreal startSeconds, qreal durationSeconds, int track, const QString &filePath);
+    void cutSelectedClip();
+    void togglePlay();
+    void stopPlay();
 
 private slots:
     void snapClipToRuler(ClipItem* clip);
+    void updatePlayhead();
 
 private:
-    QGraphicsScene *m_scene;
-    int m_pixelsPerSecond = 100;
-    int m_trackHeight = 60;
     void drawRuler();
     void drawTracks(int numTracks);
     qreal snapToSecond(qreal x) const;
+
+    QGraphicsScene *m_scene;
+    int m_pixelsPerSecond = 50;
+    int m_trackHeight = 60;
+    QGraphicsLineItem *m_playhead = nullptr;
+    QTimer *m_playTimer = nullptr;
+    qreal m_playheadPos = 0;
+    bool m_isPlaying = false;
 };
