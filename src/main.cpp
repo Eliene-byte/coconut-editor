9ecd7817f658225ff19cba3f37549f3b0415f5ec
Eliene-byte <elienedantas123456@gmail.com>
2026-04-19 14:19:26 -0300

    fix: workflow vcpkg SDL2+ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,37 @@
+#include <SDL.h>
 #include <imgui.h>
 #include <imgui_impl_sdl2.h>
 #include <imgui_impl_opengl2.h>
-#include <SDL.h>
-#ifdef _WIN32
-#include <windows.h>
-#endif
 #include <GL/gl.h>
 
-int main(int, char**){
+int main(int, char**) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("CoConut", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
-    SDL_GLContext gl = SDL_GL_CreateContext(window);
-
-    IMGUI_CHECKVERSION();
+    SDL_Window* win = SDL_CreateWindow("CoConut Editor", 100, 100, 1280, 720, SDL_WINDOW_OPENGL);
+    SDL_GLContext ctx = SDL_GL_CreateContext(win);
+    
     ImGui::CreateContext();
-    ImGui_ImplSDL2_InitForOpenGL(window, gl);
+    ImGui_ImplSDL2_InitForOpenGL(win, ctx);
     ImGui_ImplOpenGL2_Init();
-
-    bool done = false;
-    while(!done){
+    
+    bool run = true;
+    while(run) {
         SDL_Event e;
-        while(SDL_PollEvent(&e)){
+        while(SDL_PollEvent(&e)) {
             ImGui_ImplSDL2_ProcessEvent(&e);
-            if(e.type == SDL_QUIT) done = true;
+            if(e.type == SDL_QUIT) run = false;
         }
         ImGui_ImplOpenGL2_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
-
-        ImGui::Begin("CoConut Editor Pro");
-        ImGui::Text("500 Efeitos | Keyframes | Color Grading");
-        ImGui::Separator();
-        if(ImGui::Button("Timeline", {120,40})){}
-        ImGui::SameLine();
-        if(ImGui::Button("Efeitos", {120,40})){}
-        ImGui::SameLine();
-        if(ImGui::Button("Export", {120,40})){}
+        
+        ImGui::Begin("CoConut");
+        ImGui::Text("Build funcionando!");
         ImGui::End();
-
+        
         ImGui::Render();
-        glViewport(0,0,1280,720);
-        glClearColor(0.1f,0.1f,0.1f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(win);
     }
     return 0;
 }
